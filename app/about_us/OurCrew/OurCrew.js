bc3441db537f037cea5eb2cde8e68e59295a8c5f
AliReaz <reza1632731@gmail.com>
2024-08-28 20:13:25 +0200

    modify crew.js

--- app/about_us/OurCrew/OurCrew.js
+++ app/about_us/OurCrew/OurCrew.js
@@ -1,4 +1,5 @@
 import styles from "./OueCrew.module.css";
+
 export default function OurCrew() {
   // TASK - React 1 week 1
   // Create the "Our Crew section"
@@ -6,58 +7,60 @@ export default function OurCrew() {
   // Use the pictures from /public/crew
   // Some inspiration ideas found in /data/inspiration_about_us
   return (
-    <div className={styles.grid}>
-      <div>
-        <img
-          className={styles.image}
-          src="./crew/image-anousheh-ansari.png"
-          alt="anushe"
-        />
-        <p>
-          Anousheh Ansari (Persian: انوشه انصاری; née Raissyan;[2] born
-          September 12, 1966) is an Iranian American engineer and co-founder and
-          chairwoman of Prodea Systems
-        </p>
-      </div>
-      <div>
-        <img
-          className={styles.image}
-          src="./crew/image-douglas-hurley.png"
-          alt="anushe"
-        />
-        <p>
-          Douglas Gerald Hurley (born October 21, 1966) is an American engineer,
-          former Marine Corps pilot and former NASA astronaut. He piloted Space
-          Shuttle missions STS-127 (July 2009)[3] and STS-135 (July 2011), the
-          final flight of the Space Shuttle program.
-        </p>
-      </div>
-      <div>
-        <img
-          className={styles.image}
-          src="./crew/image-mark-shuttleworth.png"
-          alt="anushe"
-        />
-        <p>
-          Mark Richard Shuttleworth (born 18 September 1973) is a South African
-          and British entrepreneur who is the founder and CEO of Canonical, the
-          company behind the development of the Linux-based Ubuntu operating
-          system
-        </p>
-      </div>
+    <>
+      <div className={styles.grid}>
+        <div>
+          <img
+            className={styles.image}
+            src="./crew/image-anousheh-ansari.png"
+            alt="anushe"
+          />
+          <p>
+            Anousheh Ansari (Persian: انوشه انصاری; née Raissyan;[2] born
+            September 12, 1966) is an Iranian American engineer and co-founder
+            and chairwoman of Prodea Systems
+          </p>
+        </div>
+        <div>
+          <img
+            className={styles.image}
+            src="./crew/image-douglas-hurley.png"
+            alt="anushe"
+          />
+          <p>
+            Douglas Gerald Hurley (born October 21, 1966) is an American
+            engineer, former Marine Corps pilot and former NASA astronaut. He
+            piloted Space Shuttle missions STS-127 (July 2009)[3] and STS-135
+            (July 2011), the final flight of the Space Shuttle program.
+          </p>
+        </div>
+        <div>
+          <img
+            className={styles.image}
+            src="./crew/image-mark-shuttleworth.png"
+            alt="anushe"
+          />
+          <p>
+            Mark Richard Shuttleworth (born 18 September 1973) is a South
+            African and British entrepreneur who is the founder and CEO of
+            Canonical, the company behind the development of the Linux-based
+            Ubuntu operating system
+          </p>
+        </div>
 
-      <div>
-        <img
-          className={styles.image}
-          src="./crew/image-victor-glover.png"
-          alt="anushe"
-        />
-        <p>
-          Victor Jerome Glover (born April 30, 1976) is a NASA astronaut of the
-          class of 2013[1][2] and pilot on the first operational flight of the
-          SpaceX Crew Dragon to the International Space Station.
-        </p>
+        <div>
+          <img
+            className={styles.image}
+            src="./crew/image-victor-glover.png"
+            alt="anushe"
+          />
+          <p>
+            Victor Jerome Glover (born April 30, 1976) is a NASA astronaut of
+            the class of 2013[1][2] and pilot on the first operational flight of
+            the SpaceX Crew Dragon to the International Space Station.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
